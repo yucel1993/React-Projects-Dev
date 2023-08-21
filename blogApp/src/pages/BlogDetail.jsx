1b7c6407cc4e5e8b25ee0103853cd8680beb04b3
Burhan <115505382+yucel1993@users.noreply.github.com>
2023-08-21 22:29:30 +0200

    Update BlogDetail.jsx

--- blogApp/src/pages/BlogDetail.jsx
+++ blogApp/src/pages/BlogDetail.jsx
@@ -44,7 +44,7 @@ const BlogDetail = ({ setInfo: setUpperInfo }) => {
   const getBlog = async (id) => {
     try {
       const { data } = await axios(
-        `http://32272.fullstack.clarusway.com/api/blogs/${id}/`,
+        `https://32272.fullstack.clarusway.com/api/blogs/${id}/`,
         {
           headers: {
             Authorization: `Token ${token}`,
