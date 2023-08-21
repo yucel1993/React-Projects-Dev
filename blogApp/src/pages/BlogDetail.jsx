1483a23062a7f59ae6db6a430fc185fc296284d4
Burhan <alpagut1993@gmail.com>
2023-08-21 23:28:34 +0200

    done

--- blogApp/src/pages/BlogDetail.jsx
+++ blogApp/src/pages/BlogDetail.jsx
@@ -44,7 +44,7 @@ const BlogDetail = ({ setInfo: setUpperInfo }) => {
   const getBlog = async (id) => {
     try {
       const { data } = await axios(
-        `http://32272.fullstack.clarusway.com/api/blogs/${id}/`,
+        `${import.meta.env.VITE_BASE_URL}/api/blogs/${id}/`,
         {
           headers: {
             Authorization: `Token ${token}`,
