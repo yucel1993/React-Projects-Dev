3367c29025fd486c44fdb45e553b7fb85d4466c8
Burhan <alpagut1993@gmail.com>
2023-10-04 22:56:27 +0200

    toastify:updated and works

--- filmpire/src/main.jsx
+++ filmpire/src/main.jsx
@@ -6,6 +6,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
 import ToggleColorModeProvider from "./utills/ToggleColorMode";
+import "react-toastify/dist/ReactToastify.css";
 
 const theme = createTheme({});
 ReactDOM.createRoot(document.getElementById("root")).render(
