787fad1b3eb25697efef71e639856323803412b8
Burhan <alpagut1993@gmail.com>
2023-10-06 23:50:29 +0200

    priceWise:added

--- pricewise/app/layout.tsx
+++ pricewise/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         
-        <main className='max-w-10xl mx-auto'>
+        <main className='max-w-10xl mx-auto '>
           <Navbar/>{children}</main>
         </body>
     </html>
---
+++ pricewise/components/Searchbar.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react'
+
+const Searchbar = () => {
+    const handleSubmit=()=>{}
+
+  return (
+    <form className='flex flex-wrap gap-4 mt-12'
+    onSubmit={handleSubmit}
+    >
+        <input 
+        type='text'
+        placeholder='Enter product link'
+        className='searchbar-input'
+        />
+    </form>
+  )
+}
+
+export default Searchbar
