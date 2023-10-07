5d6c1cc970b826056cb513eec4f24708c1861695
Burhan <alpagut1993@gmail.com>
2023-10-07 16:15:24 +0200

    conection :failed -reboot

--- pricewise/components/Searchbar.tsx
+++ pricewise/components/Searchbar.tsx
@@ -1,20 +1,64 @@
 "use client"
-import React from 'react'
+
+
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import React, { useState, FormEvent } from 'react';
 
 const Searchbar = () => {
-    const handleSubmit=()=>{}
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isValidAmazonProductURL = (url: string) => {
+    try {
+      const parsedURL = new URL(url);
+      const hostname = parsedURL.hostname;
+      if (
+        hostname.includes('amazon.com') ||
+        hostname.includes('amazon.') ||
+        hostname.endsWith('amazon')
+      ) {
+        return true;
+      }
+    } catch (error) {
+      return false;
+    }
+    return false;
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+    if (!isValidLink) return alert('Please provide a valid Amazon Link');
+    try {
+      setIsLoading(true);
+      // Scrape the Product Page
+      const productData = await scrapeAndStoreProduct(searchPrompt); // Use the returned data
+      console.log(productData); // Log the scraped data
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <form className='flex flex-wrap gap-4 mt-12'
-    onSubmit={handleSubmit}
-    >
-        <input 
+    <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
+      <input
         type='text'
         placeholder='Enter product link'
         className='searchbar-input'
-        />
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+      />
+      <button
+        disabled={searchPrompt === ''}
+        type='submit'
+        className='searchbar-btn'
+      >
+        {isLoading ? 'Searching...' : 'Search'}
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
